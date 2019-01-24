60be92d7b6ca40e3399be6d053bb03e9e022f750
Mark B <mark.bosley@gmail.com>
2019-01-24 13:42:03 -0800

    remove debug

--- backend/middlewares/analytics.js
+++ backend/middlewares/analytics.js
@@ -13,11 +13,9 @@ module.exports = function(req, res, next) {
       if (err) {
         return reject()
       }
-      console.log(result)
       resolve(ua('UA-75437214-1', result, {strictCidFormat: false, geoid: req.headers['cf-ipcountry'] || null, ua: req.headers['user-agent'] || null, dr: req.params._ref || null}))
     })     
   }).then((visitor) => {
-    console.log(visitor)
     if (req.headers.referer) {
       visitor.pageview({dp: req.headers.referer.replace(/^https:\/\/wago.io/, ''), dh: 'https://wago.io'}).send()
     }
