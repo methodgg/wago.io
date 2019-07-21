89f7a1b18280c6d43789f80dbda947dd05cd4f7e
Oratory <mark.bosley@gmail.com>
2019-07-21 09:05:03 -0700

    update dev server

--- backend/api/services/html.js
+++ backend/api/services/html.js
@@ -77,7 +77,7 @@ module.exports = function (fastify, opts, next) {
           data.description = escapeHTML(doc.description.replace(/\n/g, ' ').replace(/\[\/?(?:b|center|code|color|face|font|i|justify|large|left|li|list|noparse|ol|php|quote|right|s|size|small|sub|sup|taggeduser|table|tbody|tfoot|thead|td|th|tr|u|ul|url|\*).*?\]/g, ''))
         }
         if (config.env === 'development') {
-          data.image = 'http://ubuntu:3030/html/twitter-card-image?id=' + doc._id
+          data.image = 'http://io:3030/html/twitter-card-image?id=' + doc._id
         }
         else {
           data.image = 'https://data.wago.io/html/twitter-card-image?id=' + doc._id
