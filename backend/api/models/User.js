724e025e5298e47142ebc6dc29e75b7eeb063d83
Oratory <mark.bosley@gmail.com>
2020-05-03 14:48:27 -0700

    Revert promise change to avatarURL

--- backend/api/models/User.js
+++ backend/api/models/User.js
@@ -224,14 +224,16 @@ Schema.virtual('account.total_accounts').get(function() {
 })
 
 // avatar url or default to adorable.io
-Schema.virtual('avatarURL').get(async function() {
+Schema.virtual('avatarURL').get(function() {
   if (this.profile.avatar && (this.profile.avatar.webp || this.profile.avatar.png)) {
     return this.profile.avatar
   }
   else {
     const image = require('../helpers/image')
-    this.profile.avatar = await image.avatarFromURL('https://api.adorable.io/avatars/64/' + this._id.toString() + '.png', this._id.toString(), 'adorable')
-    await this.save()
+    image.avatarFromURL('https://api.adorable.io/avatars/64/' + this._id.toString() + '.png', this._id.toString(), 'adorable').then((img) => {
+      this.profile.avatar = img
+      this.save()
+    })
     // next time, this image will be saved locally but return remote image now instead of waiting
     return {png: 'https://api.adorable.io/avatars/64/' + this._id.toString() + '.png'}
   }
