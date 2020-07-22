69742c5ce1826d8e99766dae96d69fb6b0f2693d
Oratory <mark.bosley@gmail.com>
2020-07-21 21:50:53 -0700

    fix broken profile for some users

--- frontend/src/components/core/Search.vue
+++ frontend/src/components/core/Search.vue
@@ -147,7 +147,7 @@ export default {
         opt = opt + ' Sort: ' + this.sortVal
       }
 
-      if (this.uiExpansionValue) {
+      if (this.uiExpansionValue && this.filterExpansion && this.filterExpansion.match(/all|classic|legion|bfa|sl/)) {
         opt = opt + ' Expansion: ' + this.filterExpansion
       }
 
