d54343add756b140b08e1cfd2f225cc88508ec35
Oratory <mark.bosley@gmail.com>
2021-04-30 15:29:12 -0700

    update meili index on screenshots

--- backend/api/models/Screenshot.js
+++ backend/api/models/Screenshot.js
@@ -38,5 +38,25 @@ Schema.statics.findForWago = function(id, one) {
   }
 }
 
+async function setMeiliIndex() {
+  const wago = await WagoItem.lookup(this.auraID)
+  if (wago && !(wago.hidden || wago.private || wago.encrypted || wago.restricted || wago.deleted || wago.blocked)) {
+    var meiliToDoWA = await redis.getJSON('meili:todo:weakauras') || []
+    meiliToDoWA = meiliToDoWA.filter(doc => {
+      return doc.id !== this._id
+    })
+    meiliToDoWA.push(await wago.meiliWAData)
+    redis.setJSON('meili:todo:weakauras', meiliToDoWA)
+    if (!wago._meiliWA) {
+      wago._meiliWA = true
+      await wago.save()
+    }
+  }
+}
+
+Schema.post('save', setMeiliIndex)
+Schema.post('update', setMeiliIndex)
+Schema.post('remove', setMeiliIndex)
+
 const Screenshot = mongoose.model('AuraScreenshot', Schema)
 module.exports = Screenshot
