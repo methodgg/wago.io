4ab5d499e47f6c09bcd3d5d057eac75a69403d51
Oratory <mark.bosley@gmail.com>
2021-06-17 08:09:44 -0700

    webhook update

--- backend/api/helpers/webhooks.js
+++ backend/api/helpers/webhooks.js
@@ -1,74 +1,77 @@
-const Discord = require("discord.js")
+// hex color to decimal https://www.spycolor.com/
 
 module.exports = {
   discord: {
     // broadcast to owner's discord when a new import is created
     onCreate: async (author, wago) => {
-      if (wago.private || wago.restricted || wago.encrypted) {
-        return
-      }
-      if (wago.type === 'WEAKAURA') {
-        wago.type = 'WeakAura'
-      }
-      else if (wago.type === 'CLASSIC-WEAKAURA') {
-        wago.type = 'Classic WeakAura'
-      }
-      // build message
-      const avatar = await author.avatarURL
-      const embed = new Discord.MessageEmbed()
-        .setColor('#c1272d')
-        .setTitle(`New Import: ${wago.name}`)
-        .setDescription(`${author.account.username} has created a new ${wago.type} import.`)
-        .setURL(wago.url)
-        .setAuthor(author.account.username, avatar.png, `https://wago.io${author.profile.url}`)
-        .setTimestamp()
-        .setFooter('Wago.io', 'https://media.wago.io/favicon/favicon-16x16.png')
-
-      try {
-        const wh = author.discord.webhooks.onCreate.match(/\/api\/webhooks\/(\w+)\/([-\w]+)$/)
-        const webhookClient = new Discord.WebhookClient(wh[1], wh[2])
-        webhookClient.send(embed)
-      }
-      catch (e) {
-        console.log('discord create webhook error', e.message)
-      }
-    },
-
-    onUpdate: async (author, wago) => {
-      if (wago.private || wago.restricted || wago.encrypted) {
+      if (wago.private || wago.restricted || wago.encrypted || !author.discord || !author.discord.webhooks || !author.discord.webhooks.onCreate) {
         return
       }
       let type = wago.type
-      if (type === 'WEAKAURA') {
+      if (type.match(/WEAKAURA/)) {
         type = 'WeakAura'
       }
-      else if (type === 'CLASSIC-WEAKAURA') {
-        type = 'Classic WeakAura'
-      }
-      // build message
       const avatar = await author.avatarURL
-      const embed = new Discord.MessageEmbed()
-        .setColor('#c1272d')
-        .setTitle(`Updated Import: ${wago.name}`)
-        .setDescription(`${author.account.username} has updated the ${type} import.`)
-        .setURL(wago.url)
-        .setImage(await wago.getThumbnailURL())
-        .setAuthor(author.account.username, avatar.png, `https://wago.io${author.profile.url}`)
-        .setTimestamp()
-        .setFooter('Wago.io', 'https://media.wago.io/favicon/favicon-16x16.png')
 
+      let embeds = [{
+        title: `Updated Import: ${wago.name}`,
+        type: 'rich',
+        description: `${author.account.username} has created a new ${wago.type} import.`,
+        url: wago.url,
+        timestamp: new Date(),
+        color: 12658477, // #c1272d
+        image: {
+          url: await wago.getThumbnailURL(),
+        },
+        author: {
+          name: author.account.username,
+          url: `https://wago.io${author.profile.url}`,
+          icon_url: avatar.png
+        },
+        footer: {
+          text: 'Wago.io',
+          icon_url: 'https://media.wago.io/favicon/favicon-16x16.png'
+        }
+      }]
+      const wh = author.discord.webhooks.onCreate.match(/\/api\/webhooks\/(\w+)\/([-\w]+)$/)
+      sendDiscordWebhook({embeds}, wh[1], wh[2])
+    },
+
+    onUpdate: async (author, wago) => {
+      if (wago.private || wago.restricted || wago.encrypted || !author.discord || !author.discord.webhooks || !author.discord.webhooks.onCreate) {
+        return
+      }
+      let type = wago.type
+      if (type.match(/WEAKAURA/)) {
+        type = 'WeakAura'
+      }
+      const avatar = await author.avatarURL
+
+      let embeds = [{
+        title: `Updated Import: ${wago.name}`,
+        type: 'rich',
+        description: `${author.account.username} has updated the ${type} import.`,
+        url: wago.url,
+        timestamp: new Date(),
+        color: 12658477, // #c1272d
+        image: {
+          url: await wago.getThumbnailURL(),
+        },
+        author: {
+          name: author.account.username,
+          url: `https://wago.io${author.profile.url}`,
+          icon_url: avatar.png
+        },
+        footer: {
+          text: 'Wago.io',
+          icon_url: 'https://media.wago.io/favicon/favicon-16x16.png'
+        }
+      }]
       if (wago.latestVersion.changelog.text) {
-        embed.addFields({name: 'Changelog', value: wago.latestVersion.changelog.text.replace(/\[(\w+)[^\]]*](.*?)\[\/\1]/g, '')})
+        embeds[0].description += "\n\nChangelog: " + wago.latestVersion.changelog.text.replace(/\[(\w+)[^\]]*](.*?)\[\/\1]/g, '')
       }
-
-      try {
-        const wh = author.discord.webhooks.onCreate.match(/\/api\/webhooks\/(\w+)\/([-\w]+)$/)
-        const webhookClient = new Discord.WebhookClient(wh[1], wh[2])
-        webhookClient.send(embed)
-      }
-      catch (e) {
-        console.log('discord create webhook error', e.message)
-      }
+      const wh = author.discord.webhooks.onCreate.match(/\/api\/webhooks\/(\w+)\/([-\w]+)$/)
+      sendDiscordWebhook({embeds}, wh[1], wh[2])
     },
 
     onReport: async (author, wago, moderation) => {
@@ -115,3 +118,14 @@ async function sendDiscordWebhook(data, id, token) {
   await axios.post(`https://discord.com/api/webhooks/${id}/${token}?wait=true`, data)
 }
 
+// async function modifyDiscordWebhook(string, id, token, msgID) {
+//   console.log(data)
+//   try {
+//     // let res = await axios.post(`https://discord.com/api/webhooks/${id}/${token}?wait=true`, data)
+//     let res = await axios.get(`https://discord.com/api/webhooks/${id}/${token}/messages/851682916115283989?wait=true`)
+//     console.log(res.data)
+//   }
+//   catch (e) {
+//     console.log(e.response)
+//   }
+// }
