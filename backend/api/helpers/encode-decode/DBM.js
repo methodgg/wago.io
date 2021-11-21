edf1d97178f6ba96d06162e1798a79fc9b6e76af
Oratory <mark.bosley@gmail.com>
2021-11-21 10:23:34 -0800

    dbm

---
+++ backend/api/helpers/encode-decode/DBM.js
@@ -0,0 +1,49 @@
+module.exports = {
+  typeMatch: /^DBM$/i,
+  domain: ENUM.DOMAIN.WOW,
+
+  decode: async (encodedString, exec) => {
+    // test that string matches expected regex
+    if (!encodedString.match(/^[a-zA-Z0-9\(\)]*$/)) {
+      return false
+    }
+    const lua = `
+      local encoded = "${encodedString}"
+      local success, deserialized = LibSerialize:Deserialize(LibDeflate:DecompressDeflate(LibDeflate:DecodeForPrint(encoded)))
+      return JSON:encode(deserialized)
+    `
+    try {
+      let json = await exec(lua)
+      return JSON.parse(json)
+    }
+    catch {
+      return false
+    }
+  },
+
+  encode: async (json, exec) => {
+    const lua = `
+    local t = JSON:decode("${json}")
+    return LibDeflate:EncodeForPrint(LibDeflate:CompressDeflate(LibSerialize:Serialize(t), {level = 9}))`
+    try {
+      let encodedString = await exec(lua)
+      console.log('encoded')
+      return encodedString
+    }
+    catch (e) {
+      return false
+    }
+  },
+
+  processMeta: (obj) => {
+    if (!obj || !obj.DBM || !obj.DBT) {
+      return false
+    }
+    var meta = {}
+
+    meta.name = 'DBM Profile'
+    meta.type = 'DBM'
+
+    return meta
+  }
+}
