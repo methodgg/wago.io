7cdc912d9ba56080f8ca82ee5eebd01f21f4201e
Oratory <mark.bosley@gmail.com>
2022-10-26 09:54:22 -0700

    temp fix

--- backend/api/services/lookup.js
+++ backend/api/services/lookup.js
@@ -707,8 +707,10 @@
      else if (doc.type.match(/WEAKAURA/)) {
        var json = JSON.parse(code.json)
        // check for any missing data
-       if ((code.version && (!code.encoded || !code.customCode || ((json.d.version !== code.version || json.d.url !== doc.url + '/' + code.version) || (json.c && json.c[0] && json.c[0].version !== code.version) || (json.d.semver !== code.versionString))))
-       || doc.game !== patchDates.gameVersion(json.d.tocversion)) {
+       if (
+        (code.version && (!code.encoded || !code.customCode || ((json.d.version !== code.version || json.d.url !== doc.url + '/' + code.version) || (json.c && json.c[0] && json.c[0].version !== code.version) || (json.d.semver !== code.versionString))))
+      //  || doc.game !== patchDates.gameVersion(json.d.tocversion)
+       ) {
          var q = await taskQueue.add('ProcessCode', {id: doc._id, version: code.versionString}, {priority: req.user && req.user.access.queueSkip && 2 || 5, jobId: `${doc._id}:${code.version}:${code.versionString}`})
          wagoCode.Q = q.id
        }
