964954ac449562e473400f5d98982fdf335ecd29
Mark Bosley <mark.bosley@gmail.com>
2023-05-09 16:35:40 -0700

    10.1 patch number

--- backend/api/helpers/patchDates.js
+++ backend/api/helpers/patchDates.js
@@ -20,6 +20,7 @@ var patches = [
   {date: moment('2019-12-19T03:00:00Z'), game: 'classic', patch: "WoW Classic 1.13.3", domain: 0},
   {date: moment('2019-05-17T03:00:00Z'), game: 'classic', patch: "WoW Classic 1.13.2", domain: 0},
 
+  {date: moment('2023-05-09T03:00:00Z'), game: 'df', patch: "Dragonflight 10.1.0", base: true, domain: 0},
   {date: moment('2022-11-28T03:00:00Z'), game: 'df', patch: "Dragonflight 10.0.0", base: true, domain: 0},
   {date: moment('2022-10-25T03:00:00Z'), game: 'df', patch: "Dragonflight Pre-patch 10.0.0", base: true, domain: 0},
   {date: moment('2022-09-01T03:00:00Z'), game: 'df', patch: "Dragonflight Beta 10.0.0", base: true, domain: 0},
