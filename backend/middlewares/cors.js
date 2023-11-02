ed68f92ca89e1d057d3ff52e1977badadaf15d33
Oratory <mark.bosley@gmail.com>
2023-11-02 07:07:11 -0700

    update cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
   const method = req.raw.method && req.raw.method.toUpperCase && req.raw.method.toUpperCase()
-  const allowedOrigins = [/^https:\/\/([^.]+\.)?wago.io/, /^http:\/\/io:8080/]
+  const allowedOrigins = [/^https:\/\/([^.]+\.)?wago.io/, /^http:\/\/io:8080/, /^http:\/\/localhost:\d+/]
 
   let allow = true
   if (!req.headers.origin) {
