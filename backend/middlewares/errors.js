5bec108be05eec48794d05d4cc6f12069a109969
Oratory <mark.bosley@gmail.com>
2024-08-16 06:49:09 -0700

    improve error logging

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -10,8 +10,17 @@ module.exports = function (error, req, res) {
   if (req && req.raw && req.raw.originalUrl) {
     label = label + ' ' + req.raw.originalUrl
   }
-  // log error to matomo
-  req.trackError(error, label)
+  // log error 
+  LoggedMsg.write('ERROR', error.message, {
+      message: error?.message,
+      name: error?.name,
+      stack: error?.stack,
+      config: error?.config,
+      code: error?.code,
+      status: error?.response?.status,
+      data: error?.response?.data,
+      headers: error?.response?.headers
+  })
   // send generic error to browser
   res.send('{"error": "An error has occurred."}')
 }
