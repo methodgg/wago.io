87dec7b044bc605250f96697b408fd4df46df308
Oratory <mark.bosley@gmail.com>
2024-08-26 07:33:51 -0700

    update auth api

--- backend/api/services/api.js
+++ backend/api/services/api.js
@@ -328,7 +328,7 @@ module.exports = function (fastify, opts, next) {
             }
         }
         else {
-            return res.code(401).send({ error: "invalid_request" })
+            return res.code(400).send({ error: "unknown_auth_type" })
         }
         return res.send({
             user_id: user._id,
