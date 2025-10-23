956f20967f4d752a43f312ed1e5639a94be21ac3
Oratory <mark.bosley@gmail.com>
2025-10-23 13:06:25 -0700

    fix fellowship imports in some locales

--- backend/api/helpers/encode-decode/Fellowship.js
+++ backend/api/helpers/encode-decode/Fellowship.js
@@ -18,14 +18,22 @@ module.exports = {
         const payload = raw.slice(16)
         
         const decompressed = zlib.inflateSync(payload)
-        const shifted = Buffer.from(decompressed.map(b => b + 1))
-        const jsonStr = shifted.toString("utf8")
+        const shifted = [...Buffer.from(decompressed)].map(b => {
+          if (b >= 0x1f) return String.fromCharCode(b+1);
+          // console.log('invalid value', b)
+          return `$0x${b.toString(16).padStart(2,'0')}$`;
+        }).join('');
+
+        const jsonStr = shifted.toString("latin1")
+        
+        // console.log(jsonStr)
         const obj = losslessJSON.parse(jsonStr)
 
         obj.vheader = header.toString("base64")
         return obj
       }
       catch (e) {
+        console.log(e)
         return false
       }
     },
@@ -35,9 +43,13 @@ module.exports = {
       const header = Buffer.from(obj.vheader, "base64")
       delete obj.vheader
       
-      const updatedStr = losslessJSON.stringify(obj)    
-      const jsonBuffer = Buffer.from(updatedStr, "utf8");
-      const shifted = Buffer.from(jsonBuffer.map(b => b - 1));
+      const updatedStr = losslessJSON.stringify(obj).replace(/\$0x([0-9A-F]{2})\$/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)))
+      const jsonBuffer = Buffer.from(updatedStr, 'latin1');
+      const shifted = Buffer.from(jsonBuffer.map(b => {
+        if (b >= 0x20) return b - 1
+        // console.log('invalid value back', b)
+        return b
+      }))
       const recompressed = zlib.deflateSync(shifted);
       const raw = Buffer.concat([header, recompressed]);
       return raw.toString("base64");
@@ -63,5 +75,5 @@ module.exports = {
   
     //   return {code}
     // }
-  }
+}
   
