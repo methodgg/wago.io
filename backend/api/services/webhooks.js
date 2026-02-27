1ecb2518b5b6b5f5a94028654bb6e240b9249b1f
Oratory <mark.bosley@gmail.com>
2026-02-27 15:09:16 +0000

    delete acct webhook

--- backend/api/services/webhooks.js
+++ backend/api/services/webhooks.js
@@ -3,84 +3,105 @@ const image = require('../helpers/image')
 const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports = function (fastify, opts, next) {
-  // Allow github webhook to post and trigger addon update task
-  fastify.post('/tasks/addons', async (req, res) => {
-    if (!req.headers['user-agent'].match(/^GitHub-Hookshot\//) ) {
-      return res.code(403).send({error: 'invalid_access'})
-    }
-    await taskQueue.add('UpdateLatestAddonReleases', null, {priority: 3})
-    res.send({done: true})
-  })
-
-  fastify.post('/webhooks/wago-account', async (req, res) => {
-    const payloadString = phpJsonEncode(req.body)
-
-    const computedSignature = crypto
-        .createHmac('sha256', config.wagoAuthKey)
-        .update(payloadString, 'utf8')
-        .digest('hex')
-
-    if (computedSignature !== req.headers['signature']) {
-        LoggedMsg.write('FAILED_AUTH_WH', "invalid webhook signature", {
-            type: req.body.type,
-            body: req.body,
-            computedSignature,
-            expectedSignature: req.headers['signature']
-        })
-        return res.status(401).send({error: 'invalid_signature'})
-    }
-
-    if (req.body.type !== 'user.upserted') {
-        LoggedMsg.write('FAILED_AUTH_WH', "invalid type", {
-            type: req.body.type,
-            body: req.body
-        })
-        return res.status(400).send({error: 'unknown_type'})
-    }
-
-    const payload = req.body.payload
-    let query
-    try {
-        query = { _id: ObjectId(payload.id) }
-    }
-    catch {
-        query = { "wagoAuth.id": payload.id }
-    }
-
-    const user = await User.findOne(query)
-    if (!user) {
-        return res.send({error: 'new_user'})
-    }
-
-    if (payload.username) {
-        user.account.username = payload.username
-        user.search.username = payload.username.toLowerCase()
-    }
-    if (payload.avatar) {
-        user.profile.avatar = await image.avatarFromURL(payload.avatar, user._id.toString())
-    }
-
-    user.wagoAuth = {
-        id: payload.id,
-        name: payload.username,
-    }
-
-    if (payload.socialLogins?.discord) {
-        user.discord.id = payload.socialLogins.discord
-    }
-    if (payload.socialLogins?.battlenet) {
-        user.battlenet.id = payload.socialLogins.battlenet
-        if (payload.wowChars?.length) {
-            user.battle.net.characters = payload.wowChars
+    // Allow github webhook to post and trigger addon update task
+    fastify.post('/tasks/addons', async (req, res) => {
+        if (!req.headers['user-agent'].match(/^GitHub-Hookshot\//) ) {
+            return res.code(403).send({error: 'invalid_access'})
         }
-    }
-    
-    user.roles.subscriber = payload.benefits?.includes('green_name_tag') || payload.benefits?.includes('golden_name_tag')
-    user.roles.gold_subscriber = payload.benefits?.includes('golden_name_tag')
-    user.account.verified_human = user.account.verified_human || user.roles.subscriber || user.roles.gold_subscriber || Boolean(payload.email_verified_at)
+        await taskQueue.add('UpdateLatestAddonReleases', null, {priority: 3})
+        res.send({done: true})
+    })
 
-    await user.save()
-    res.send({done: true})
+    fastify.post('/webhooks/wago-account', async (req, res) => {
+        const payloadString = phpJsonEncode(req.body)
+
+        const computedSignature = crypto
+            .createHmac('sha256', config.wagoAuthKey)
+            .update(payloadString, 'utf8')
+            .digest('hex')
+
+        if (computedSignature !== req.headers['signature']) {
+            LoggedMsg.write('FAILED_AUTH_WH', "invalid webhook signature", {
+                type: req.body.type,
+                body: req.body,
+                computedSignature,
+                expectedSignature: req.headers['signature']
+            })
+            return res.status(401).send({error: 'invalid_signature'})
+        }
+
+        const payload = req.body.payload
+        if (req.body.type === 'user.upserted') {
+            let query
+            try {
+                query = { _id: ObjectId(payload.id) }
+            }
+            catch {
+                query = { "wagoAuth.id": payload.id }
+            }
+
+            const user = await User.findOne(query)
+            if (!user) {
+                return res.send({error: 'new_user'})
+            }
+
+            if (payload.username) {
+                user.account.username = payload.username
+                user.search.username = payload.username.toLowerCase()
+            }
+            if (payload.avatar) {
+                user.profile.avatar = await image.avatarFromURL(payload.avatar, user._id.toString())
+            }
+
+            user.wagoAuth = {
+                id: payload.id,
+                name: payload.username,
+            }
+
+            if (payload.socialLogins?.discord) {
+                user.discord.id = payload.socialLogins.discord
+            }
+            if (payload.socialLogins?.battlenet) {
+                user.battlenet.id = payload.socialLogins.battlenet
+                if (payload.wowChars?.length) {
+                    user.battle.net.characters = payload.wowChars
+                }
+            }
+            
+            user.roles.subscriber = payload.benefits?.includes('green_name_tag') || payload.benefits?.includes('golden_name_tag')
+            user.roles.gold_subscriber = payload.benefits?.includes('golden_name_tag')
+            user.account.verified_human = user.account.verified_human || user.roles.subscriber || user.roles.gold_subscriber || Boolean(payload.email_verified_at)
+
+            await user.save()
+            res.send({done: true})
+        }
+        else if (req.body.type === 'user.deleted') {
+            let query
+            try {
+                query = { _id: ObjectId(payload.id) }
+            }
+            catch {
+                query = { "wagoAuth.id": payload.id }
+            }
+
+            const user = await User.findOne(query)
+            if (!user) {
+                return res.send({error: 'user_not_found'})
+            }
+
+            await WagoItem.deleteMany({_userId: user._id})
+            await Comments.deleteMany({_userId: user._id})
+            await user.remove()
+            
+            res.send({done: true})
+        }   
+        else {        
+            LoggedMsg.write('FAILED_AUTH_WH', "invalid type", {
+                type: req.body.type,
+                body: req.body
+            })
+            return res.status(400).send({error: 'unknown_type'})
+        }
   })
 
   next()
